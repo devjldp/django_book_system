85210e1e5ea4bc0503a6285451185be46a13b902
jldpDev <joseluisdp297@gmail.com>
2025-12-17 08:33:04 +0000

    Create model book

--- book_system/books/models.py
+++ book_system/books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+#Create your models here
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
