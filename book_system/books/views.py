b4a2cded661a37dd444864b1b513bfd587a134b9
jldpDev <joseluisdp297@gmail.com>
2025-12-17 09:18:17 +0000

    create book_create view

--- book_system/books/views.py
+++ book_system/books/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Book
+from .forms import BookForm
 
 # Create your views here.
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Book created successfully!')
+            return redirect('book_list')
+    else:
+        form = BookForm()
+
+    context = {
+        'form': form,
+        'action': 'Create'
+    }
+    return render(request, 'books/book_form.html', context )
